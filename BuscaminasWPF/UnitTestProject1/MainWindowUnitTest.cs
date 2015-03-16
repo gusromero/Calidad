b58c48f55aa5f5c88ded4e8e7811a8b99d441136
gusromero <gusromero@gmail.com>
2015-03-16 23:11:53 +0100

    Added MainWindowUnitTest

---
+++ BuscaminasWPF/UnitTestProject1/MainWindowUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class MainWindowUnitTest
+    {
+        [TestMethod]
+        public void CleanFieldTestMethod()
+        {
+            MinesweeperWPF.MainWindow window = new MinesweeperWPF.MainWindow();
+            window.Size = 10;
+
+            window.CleanField();
+
+            Assert.AreEqual(0, window.Field.ColumnDefinitions.Count);
+            Assert.AreEqual(0, window.Field.RowDefinitions.Count);
+            Assert.AreEqual(0, window.Field.Children.Count);
+        }
+
+        [TestMethod]
+        public void GenerateFieldTestMethod()
+        {
+            MinesweeperWPF.MainWindow window = new MinesweeperWPF.MainWindow();
+            window.Size = 10;
+            window.CleanField();
+            window.GenerateField();
+
+            Assert.AreEqual(10, window.Field.ColumnDefinitions.Count);
+            Assert.AreEqual(10, window.Field.RowDefinitions.Count);
+            Assert.AreEqual(100, window.Field.Children.Count);
+
+        }
+
+    }
+}
