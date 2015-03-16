2a1e0934581ce98cc5deef3ab8032ffe454ad21d
gusromero <gusromero@gmail.com>
2015-03-16 22:52:17 +0100

    Added Test project

---
+++ BuscaminasWPF/UnitTestProject1/TileControlUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TileControlUnitTest
+    {
+        [TestMethod]
+        public void DiscoverEventTestMethod()
+        {
+            TileControlLib.TileControl control = new TileControlLib.TileControl();
+            control.State = TileControlLib.TileControlState.Covered;
+            control.Discover += delegate(object sender,
+                System.Windows.RoutedPropertyChangedEventArgs<TileControlLib.TileControlState> e)
+            {
+                if (e.NewValue == e.OldValue)
+                    Assert.Fail();
+                else
+                    Assert.IsTrue(true);
+            };
+            control.State = TileControlLib.TileControlState.Bomb;
+        }
+
+    }
+}
