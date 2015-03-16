0115528a14ad3b429df47edac1b718e72cb00f45
gusromero <gusromero@gmail.com>
2015-03-16 23:40:31 +0100

    Added IGenerator (II)

---
+++ BuscaminasWPF/MinesweeperWPF/IGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinesweeperWPF
+{
+    public interface IGenerator
+    {
+        int Next(int number);
+    }
+}
